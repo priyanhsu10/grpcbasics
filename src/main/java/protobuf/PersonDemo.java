7e5aac8c0b6f7e19fc003da8302ef517e76754fe
priyanshu10 <priyanshuparate@gmail.com>
2023-11-17 21:57:13 +0530

    protobuf types first module

---
+++ src/main/java/protobuf/PersonDemo.java
@@ -0,0 +1,35 @@
+package protobuf;
+import com.pro.grpc.models.Person;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PersonDemo {
+    public static void main(String[] args) throws IOException {
+//----------basics
+        Person priyanshu = Person.newBuilder()
+                .setAge(10).setName("priyanshu").build();
+//        Person priyanshu2 = Person.newBuilder()
+//                .setAge(10).setName("Priyanshu").build();
+//
+//        //we cannot ovveride the equals method instead of the implement though comparable interface
+//        System.out.println(priyanshu.equals(priyanshu2));
+//
+//
+
+//---------------------
+      //  serialization and deserialization
+//        Path path= Paths.get("priyanshu.ser");
+//        Files.write(path,priyanshu.toByteArray());
+
+        byte[] bytes = Files.readAllBytes(Path.of("priyanshu.ser"));
+
+        Person newPriyanshu= Person.parseFrom(bytes);
+        System.out.println(newPriyanshu);
+
+
+    }
+}
